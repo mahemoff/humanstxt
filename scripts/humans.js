a50daa05fcf81c5e8e9635d69f602fc3e8eb51e7
Joachim <joachim.robert@gmail.com>
2018-05-07 16:43:03 +0200

    I may not have needed a separate function for the url request

--- scripts/humans.js
+++ scripts/humans.js
@@ -1,42 +1,33 @@
 function loadHumans(site, path, success, error) {
-  humansLink = site + path;
+  var url = site + path;
 
   if(u.protocol.indexOf("http") !== -1) { // HTTP(S) links only
-    request(humansLink, function (response, err, status) {
-      if (status && status == 404) {
-        step += 1;
-        return callHumans();
-      } else if (err) {
-        error(site, humansLink);
-      } else {
-        success(response, humansLink);
-      }
+
+    var myHeaders = new Headers({
+      "Content-Type": "text/plain; charset=utf-8",
+      "Accept-Charset": "utf-8"
     });
+    var myInit = { method: 'GET', headers: myHeaders };
+
+    fetch(url, myInit).then(function(response) {
+      var contentType = response.headers.get("content-type");
+      console.log(response);
+      if(contentType && contentType.indexOf("text/plain") !== -1) {
+        return response.text().then(function(content) {
+          return success(content, url);
+        });
+      } else {
+        if (response.status == 404) {
+          step += 1;
+          return callHumans();
+        } else {
+          error(site, url);
+        }
+      }
+    }).catch(function(error) {
+      console.log("Il y a eu un problème avec l’opération fetch: " + error.message);
+      error(site, url);
+    });
+
   }
 }
-
-function request(url, next) {
-
-  var myHeaders = new Headers({
-    "Content-Type": "text/plain; charset=utf-8",
-    "Accept-Charset": "utf-8"
-  });
-  var myInit = { method: 'GET', headers: myHeaders };
-
-  fetch(url, myInit).then(function(response) {
-    var contentType = response.headers.get("content-type");
-    console.log(response);
-    if(contentType && contentType.indexOf("text/plain") !== -1) {
-      return response.text().then(function(content) {
-        return next(content)
-      });
-    } else {
-      return next(null, new Error("There was an error of some sort."), response.status);
-    }
-  }).catch(function(error) {
-    console.log("Il y a eu un problème avec l’opération fetch: " + error.message);
-    return next(null, new Error("There was an error of some sort: " + error.message));
-  });
-
-}
-
